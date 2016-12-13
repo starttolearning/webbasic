c5c5fe9d06665e25fed739028e6c6d51d0f83b2f
Wilton Lee <wilton_lee@163.com>
2016-12-13 21:02:24 +0800

    jquery load callback function added to the perivous commit

--- index.php
+++ index.php
@@ -14,26 +14,35 @@
         Last Name:<br/><input type="text" id="lastName" /> <span id="lastNameHelpDiv" ></span> <br/>
         Email:<br/><input type="text" id="email" /><span id="emailHelpDiv" ></span>  <br/>
         Income:<br/><input type="text" id="income" /><span id="incomeHelpDiv" ></span>  <br/>
-        
-        <div title="div" id="resultDiv"></div>
-    </body>
-    <!-- include the local jquery library-->
-    <script type="text/javascript" src="jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            var textBoxs =  $('input[type="text"]');
-            
-            textBoxs.focus(function () {
-                var helpDiv = $(this).attr('id');
-                $('#'+helpDiv+'HelpDiv').load( 'get-help-text.php', { HelpTextKey : helpDiv } );
-            }); 
-            
-            textBoxs.blur(function () {
-                var helpDiv = $(this).attr('id') + 'HelpDiv';
-                $('#'+helpDiv).html('');
-            });
+
+        <div id="divStatus"></div>
+</body>
+<!-- include the local jquery library-->
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        var textBoxs = $('input[type="text"]');
+
+        textBoxs.focus(function () {
+            var helpDiv = $(this).attr('id');
+            $('#' + helpDiv + 'HelpDiv').load('get-help-text.php', {HelpTextKey: helpDiv },
+                    function (response, status, xhr) {
+                        if (status == 'error') {
+                            var statusMessage = 'status = ' + xhr.status + '<br/>';
+                            statusMessage += 'status text = ' + xhr.statusText + '<br/>';
+                            statusMessage += 'response = ' + response;
+
+                            $('#divStatus').html(statusMessage);
+                        }
+                    });
         });
-    </script>
+
+        textBoxs.blur(function () {
+            var helpDiv = $(this).attr('id') + 'HelpDiv';
+            $('#' + helpDiv).html('');
+        });
+    });
+</script>
 
 
 </html>
