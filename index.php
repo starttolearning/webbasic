6dcc829bd0f936d57323dba65c87f4335d8d99fb
Wilton Lee <wilton_lee@163.com>
2016-12-15 11:10:16 +0800

    Insert entry to the Database through ajax, importants worth to mention: * PHP array to ajax response JSON format   avoid to add index: []=array('id'=>1,'name'=>'wilton')   instead using []=array('key value pair'); * AJAX insert entry to the database then output the all of   entry from the database.

--- index.php
+++ index.php
@@ -3,48 +3,97 @@
     <head>
         <meta charset="GBK">
         <title>jQuery Learning</title>
-        <style>
-
-        </style>
     </head>
     <body>
         <?php
         ?>
-        First Name:<br/><input type="text" id="firstName" /> <span id="firstNameHelpDiv" ></sapn> <br/>
-        Last Name:<br/><input type="text" id="lastName" /> <span id="lastNameHelpDiv" ></span> <br/>
-        Email:<br/><input type="text" id="email" /><span id="emailHelpDiv" ></span>  <br/>
-        Income:<br/><input type="text" id="income" /><span id="incomeHelpDiv" ></span>  <br/>
+        <table border="1" style="border-collapse:collapse;">
+            <tr>
+                <td>Name</td>
+                <td><input type="text" id="txtName"/></td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td><input type="text" id="txtGender"/></td>
+            </tr>
+            <tr>
+                <td>Salary</td>
+                <td><input type="text" id="txtSalary"/></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="button" id="btnInsert" value="Insert Employee" /></td>
+            </tr>
+        </table>
+        
+        <br/><br/>
+        <table id="tblEmployees" border="1" style="border-collapse:collapse;">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Salary</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
 
-        <div id="divStatus"></div>
-</body>
-<!-- include the local jquery library-->
-<script type="text/javascript" src="jquery.min.js"></script>
-<script type="text/javascript">
-    $(document).ready(function () {
-        var textBoxs = $('input[type="text"]');
 
-        textBoxs.focus(function () {
-            var helpDiv = $(this).attr('id');
-            // Using get() other than load()
-            $.get('get-help-text.php', {HelpTextKey: helpDiv }, function ( response, status, xhr ) {
-                var jqueryXML = $(response);
-                var textElement = jqueryXML.find('text');
-                $('#' + helpDiv + 'HelpDiv').html(textElement.text());
+        <div id="divResult"></div>
+    </body>
+    <!-- include the local jquery library-->
+    <script type="text/javascript" src="jquery3.1.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready( function () {
+            $('#btnInsert').click(function () {
+                var employee = {};
+                employee.name = $('#txtName').val();
+                employee.gender = $('#txtGender').val();
+                employee.salary = $('#txtSalary').val();
                 
-                var statusMessage = 'status : ' + xhr.responseType;
+                $.ajax({
+                   url: "insert-employee.php",
+                   method : 'post',
+                   data: {
+                       employee : JSON.stringify(employee),
+                       action : 'insert_employee'
+                   },
+                   success: function () {
+                        getAllEmployees();
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+            });
+            
+            function getAllEmployees(){
+                $.ajax({
+                   url: "insert-employee.php",
+                   method : 'post',
+                   dataType: 'json',
+                   data: { action : 'get_all_employees' },
+                   success: function (data) {
+                       var employeeTable =  $('#tblEmployees tbody');
+                       console.log(JSON.stringify( data));
+                        $(data).each( function (index, emp) {
+                            employeeTable.append(
+                                    '<tr><td>'+emp.id+'</td>'+
+                                    '<td>'+emp.name+'</td>'+
+                                    '<td>'+emp.gender+'</td>'+
+                                    '<td>'+emp.salary+'</td></tr>'
+                                );
+                        });
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+            }
                 
-                $('#divStatus').html(statusMessage);
-                console.log(xhr);
-                
-            },'xml');
-        });
-
-        textBoxs.blur(function () {
-            var helpDiv = $(this).attr('id') + 'HelpDiv';
-            $('#' + helpDiv).html('');
-        });
-    });
-</script>
+            });
+    </script>
 
 
 </html>
---
+++ requestbin.php
@@ -0,0 +1,4 @@
+<?php
+    $result = file_get_contents('http://requestb.in/1jpzcc61');
+    echo $result;
+?>
